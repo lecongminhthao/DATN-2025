b2026bb48178d0d3568ea55a32dda59b10520a09
lecongminhthao <lecongminhthao2004@gmail.com>
2025-05-29 23:22:02 +0700

    API thêm sửa và xóa trong ProductController

--- backend/admin/controller/ProductController.js
+++ backend/admin/controller/ProductController.js
@@ -62,3 +62,64 @@ exports.updateProduct = async (req, res) => {
     });
   }
 };
+// sửa sản phẩm
+exports.updateProduct = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const { name, status, importPrice, salePrice, discountPrice } = req.body;
+    const existingProduct = await checkProdcutNameExists(name);
+    if (existingProduct && existingProduct._id.toString() !== id) {
+      return res.status(400).json({ message: "Tên sản phẩm đã tồn tại" });
+    }
+    const updatedProduct = await Product.findByIdAndUpdate(
+      id,
+      {
+        name,
+        status,
+        importPrice,
+        salePrice,
+        discountPrice,
+      },
+      { new: true }
+    );
+    if (!updatedProduct) {
+      return res.status(404).json({ message: "Sản phẩm không tồn tại" });
+    }
+    res.status(200).json(updatedProduct);
+  } catch (error) {
+    res.status(500).json({
+      message: "Cập nhật sản phẩm thất bại",
+    });
+  }
+};
+//xóa sản phẩm
+
+exports.deleteProduct = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const product = await Product.findByIdAndDelete(id);
+    if (!product) {
+      return res.status(404).json({ message: "Sản phẩm không tồn tại" });
+    }
+    res.status(200).json({ message: "Sản phẩm đã được xóa thành công" });
+  } catch (error) {
+    res.status(500).json({
+      message: "Xóa sản phẩm thất bại",
+    });
+  }
+};
+// lấy sản phẩm theo id
+exports.getProductById = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const product = await Product.findById(id);
+    if (!product) {
+      return res.status(404).json({ message: "Sản phẩm không tồn tại" });
+    }
+    res.status(200).json(product);
+  } catch (error) {
+    res.status(500).json({
+      message: "Lấy sản phẩm thất bại",
+    });
+  }
+};
